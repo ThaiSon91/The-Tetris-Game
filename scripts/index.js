326a6756a35c2d41e8acc6609050907a5d50c9c0
ThaiSonDev <sonthaidev91@gmail.com>
2023-03-28 22:07:44 +0700

    the game is not working

--- scripts/index.js
+++ scripts/index.js
@@ -12,6 +12,13 @@ const COLOR_MAPPING = [
   "white",
 ];
 
+const KEY_CODES = {
+  LEFT: "ArrowLeft",
+  RIGHT: "ArrowRight",
+  UP: "ArrowUp",
+  DOWN: "ArrowDown",
+};
+
 const BRICK_LAYOUT = [
   [
     [
@@ -223,10 +230,184 @@ class Brick {
     this.id = id;
     this.layout = BRICK_LAYOUT[id];
     this.activeIndex = 0;
+    this.colPos = 3;
+    this.rowPos = 3;
   }
+  //draw brick
+  draw() {
+    for (let row = 0; row < this.layout[this.activeIndex].length; row++)
+      for (let col = 0; col < this.layout[this.activeIndex][0].length; col++) {
+        if (this.layout[this.activeIndex][row][col] != WHITE_COLOR_ID) {
+          board.drawCell(col + this.colPos, row + this.rowPos, this.id);
+        }
+      }
+  }
+  //clear
+  clear() {
+    for (let row = 0; row < this.layout[this.activeIndex].length; row++)
+      for (let col = 0; col < this.layout[this.activeIndex][0].length; col++) {
+        if (this.layout[this.activeIndex][row][col] != WHITE_COLOR_ID) {
+          board.drawCell(col + this.colPos, row + this.rowPos, WHITE_COLOR_ID);
+        }
+      }
+  }
+  //move left
+  moveLeft() {
+    if (
+      !this.checkCollision(
+        this.rowPos,
+        this.colPos - 1,
+        this.layout[this.activeIndex]
+      )
+    ) {
+      this.clear();
+      this.colPos--;
+      this.draw();
+    }
+  }
+  //move right
+  moveRight() {
+    if (
+      !this.checkCollision(
+        this.rowPos,
+        this.colPos + 1,
+        this.layout[this.activeIndex]
+      )
+    ) {
+      this.clear();
+      this.colPos++;
+      this.draw();
+    }
+  }
+  //move down
+  moveDown() {
+    if (
+      !this.checkCollision(
+        this.rowPos + 1,
+        this.colPos,
+        this.layout[this.activeIndex]
+      )
+    ) {
+      this.clear();
+      this.rowPos++;
+      this.draw();
+
+      return;
+    }
+
+    this.handleLanded();
+    generateNewBrick();
+  }
+  //rotate
+  rotate() {
+    if (
+      !this.checkCollision(
+        this.rowPos,
+        this.colPos,
+        this.layout[(this.activeIndex + 1) % 4]
+      )
+    ) {
+      this.clear();
+      this.activeIndex = (this.activeIndex + 1) % 4;
+      /**
+       * activeindex = 0
+       * 0 + 1 = 1 % 4 ==> 1
+       *
+       * activeIndex = 3
+       * 3 + 1 = 4 % 4 ==> 0
+       *
+       * **/
+      this.draw();
+    }
+  }
+  //check position future
+  checkCollision(nextRow, nextCol, nextLayout) {
+    if (nextCol < 0) return true;
+    for (let row = 0; row < nextLayout.length; row++) {
+      for (let col = 0; col < nextLayout[0].length; col++) {
+        if (nextLayout[row][col] !== WHITE_COLOR_ID && nextRow >= 0) {
+          if (
+            col + nextCol < 0 ||
+            col + nextCol >= COLS ||
+            row + nextRow >= ROWS ||
+            board.grid[row + nextRow][col + nextCol] !== WHITE_COLOR_ID
+          )
+            return true;
+        }
+      }
+    }
+    return false;
+  }
+  handleLanded() {
+    if (this.rowPos <= 0) {
+      board.handleGameOver();
+      return;
+    }
+
+    for (let row = 0; row < this.layout[this.activeIndex].length; row++) {
+      for (let col = 0; col < this.layout[this.activeIndex][0].length; col++) {
+        if (this.layout[this.activeIndex][row][col] !== WHITE_COLOR_ID) {
+          board.grid[row + this.rowPos][col + this.colPos] = this.id;
+        }
+      }
+    }
+    board.handleCompleteRows();
+    board.drawBoard();
+  }
+}
+//create brick ramdom
+function generateNewBrick() {
+  brick = new Brick(Math.floor(Math.random() * 10) % BRICK_LAYOUT.length);
 }
 
 board = new Board(ctx);
 board.drawBoard();
+
+document.getElementById("play").addEventListener("click", () => {
+  board.reset();
+  board.isPlaying = true;
+  generateNewBrick();
+  const refresh = setInterval(() => {
+    if (!board.gameOver) {
+      brick.moveDown();
+    } else {
+      clearInterval(refresh);
+    }
+  }, 1000);
+});
+
+//brick = new Brick(0);
+// brick.draw();
+// brick.rotate();
+// brick.moveLeft();
+// brick.moveDown();
+// brick.moveRight();
 // board.drawCell(1, 1, 1);
-console.log(board.grid);
+
+//move after 1second
+// setInterval(() => {
+//   brick.moveDown();
+// }, 1000);
+
+document.addEventListener("keydown", (e) => {
+  if (!board.gameOver && board.isPlaying) {
+    console.log({ e });
+    switch (e.code) {
+      case KEY_CODES.LEFT:
+        brick.moveLeft();
+        break;
+      case KEY_CODES.RIGHT:
+        brick.moveRight();
+        break;
+      case KEY_CODES.DOWN:
+        brick.moveDown();
+        break;
+      case KEY_CODES.UP:
+        brick.rotate();
+        break;
+      default:
+        break;
+    }
+  }
+});
+console.table(board.grid);
